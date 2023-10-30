03d0857f7fb16ae8c6cbb04282fec566c97f8e81
DavePhil <dave.chedjoun@facsciences-uy1.cm>
2023-10-30 15:58:34 +0100

    Implémentation de la classe cliente(Main) et finalisation

--- composite_2/src/Main.java
+++ composite_2/src/Main.java
@@ -1,5 +1,21 @@
 public class Main {
     public static void main(String[] args) {
+        Section section = new Section();
+        final int[] i = {1}; // tableau pour attribuer des valeurs aux exécutions
 
+        Texte sousSection1 = new Section("section1", "This is section1");
+        Texte sousSection2 = new Section("section1", "This is section2");;
+        Texte paragraph = new Paragraphe("Paragraphe1", "This is paragraph1");
+
+        section.ajout(sousSection1);
+        section.ajout(paragraph);
+        section.ajout(sousSection2);
+
+        section.sousSection.forEach((sec) ->{
+            System.out.println("===== " + i[0] + ". =====");
+            sec.get();
+            System.out.println("Size "+ sec.longueur());
+            i[0]++;
+        });
     }
 }
