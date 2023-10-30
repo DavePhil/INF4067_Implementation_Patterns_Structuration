f48dde1dc14cc47b0cba28282bc0d601c5d12a45
DavePhil <dave.chedjoun@facsciences-uy1.cm>
2023-10-30 12:33:15 +0100

    Écriture de la classe Main et finalisation

--- adapter_3/src/Main.java
+++ adapter_3/src/Main.java
@@ -4,6 +4,11 @@ Classe cliente qui utilise les objets de l'interface
  */
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println("*************** Debut ***************");
+        float coteCarre = 25;
+        ICarre icarre = new AdaptRectangle(coteCarre);
+        System.out.println("Aire: "+icarre.aire());
+        System.out.println("Perimetre: "+icarre.perimetre());
+        System.out.println("**************** Fin ****************");
     }
 }
