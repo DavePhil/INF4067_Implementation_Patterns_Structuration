ba3e2df735dc46b539297672f5582e6eb142ca93
DavePhil <dave.chedjoun@facsciences-uy1.cm>
2023-11-13 15:09:17 +0100

    Redaction de la classe Cliente

--- decorator_1/src/Main.java
+++ decorator_1/src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("*************** Debut ***************");
+        Fenetre fenetreA = new FenetreSimple();
+        DecorateurFenetre fenetreB = new DecorateurFenetreVertical(fenetreA);
+        System.out.println("============ 1 decoration ============");
+        fenetreB.decrire();
+        fenetreB.dessinerCurseur();
+        System.out.println("============ 2 decoration ============");
+        DecorateurFenetre fenetreC= new DecorateurFenetreHorizontal(fenetreB);
+        fenetreC.decrire();
+        fenetreC.dessinerCurseur();
+        System.out.println("**************** Fin ****************");
     }
 }
