8afe196a3301ee4ee4f7f7e4eb028ee688470b45
DavePhil <dave.chedjoun@facsciences-uy1.cm>
2023-11-20 13:03:35 +0100

    Test de la couleur Yellow

--- bridge_2/src/Main.java
+++ bridge_2/src/Main.java
@@ -2,9 +2,11 @@ public class Main {
     public static void main(String[] args) {
         Shape rectangle = new Rectangle(new RedColor());
         Shape circle = new Circle(new BlueColor());
+        Shape circle1 = new Circle(new YellowColor());
         System.out.println("*************** Debut ***************");
         rectangle.colorIt();
         circle.colorIt();
+        circle1.colorIt();
         System.out.println("**************** Fin ****************");
     }
 }
